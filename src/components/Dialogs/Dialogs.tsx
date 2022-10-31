1e369d8f46dbb804fbeebf2a1a0c01e9a14de01b
admin <krivalcevich@gmail.com>
2022-10-31 17:44:03 +0300

    21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import s from '../Dialogs/Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            я диалог
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Pavel</div>
+                <div className={s.dialog}>Andrew</div>
+                <div className={s.dialog}>Dima</div>
+                <div className={s.dialog}>Vlad</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}> How are you</div>
+                <div className={s.message}>Yo</div>
+            </div>
+
         </div>
     );
 };
